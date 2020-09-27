ed77dffe96caec528c1c5d2e00c000b7208fdacb
Rajat Roy <rajatr62@gmail.com>
2020-09-27 18:24:17 +0530

    reduced recommended movies limit to two

--- python_code/movie_recommender/recommender.py
+++ python_code/movie_recommender/recommender.py
@@ -20,7 +20,7 @@ def get_recommendation(movie_title):
 		idx = idx_arr[0]
 		sim_scores = list(enumerate(sim_mat[idx]))
 		sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-		sim_scores = sim_scores[1:6]
+		sim_scores = sim_scores[1:3]
 		movie_indices = list([i[0] for i in sim_scores])
 		# FIXME: some problem with saved index. check it out. currently taking series index instead of saved index
 		# rec_movies = list(indices[indices['index'].isin(movie_indices)]['original_title'].values)
